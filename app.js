b89013ebfff250f637eaaffc67da8e0d32e94be9
Sammaelgar1 <124628973+Sammaelgar1@users.noreply.github.com>
2025-09-22 14:13:46 -0600

    Update app.js
    Agregar un amigo a la lista

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 // Array para almacenar los amigos
 let amigos = [];
 
+// Función para agregar un amigo a la lista
+function agregarAmigo() {
+    const input = document.getElementById("amigo");
+    const nombre = input.value.trim();
+
+    if (nombre === "") {
+        alert("Por favor, ingresa un nombre válido.");
+        return;
+    }
+
+
 
 
 
